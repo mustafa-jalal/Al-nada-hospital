a4544230191f23b74db529febf7e23da3cc35235
mustafa <mustafa.jalal2@gmail.com>
2024-11-26 16:29:33 +0200

    comment not implemented code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -320,8 +320,8 @@
         <li><a href="/dashboard"><i class="fas fa-tachometer-alt"></i> لوحة التحكم</a></li>
         <li><a href="/patients"><i class="fas fa-user-injured"></i> المرضى</a></li>
         <li><a href="/visits"><i class="far fa-calendar-alt"></i> الزيارات</a></li>
-        <li><a href="/reports"><i class="fas fa-chart-bar"></i> التقارير</a></li>
-        <li><a href="/settings"><i class="fas fa-cog"></i> الإعدادات</a></li>
+{{--        <li><a href="/reports"><i class="fas fa-chart-bar"></i> التقارير</a></li>--}}
+{{--        <li><a href="/settings"><i class="fas fa-cog"></i> الإعدادات</a></li>--}}
         <li><a href="/logout"><i class="fas fa-sign-out-alt"></i> تسجيل الخروج</a></li>
     </ul>
 </div>
